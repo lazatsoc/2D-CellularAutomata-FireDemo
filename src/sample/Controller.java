5d1a9cd524d3f91f23981efb66eb82805929cab5
Lazaros Tsochatzidis <ltsochat@ee.duth.gr>
2015-05-04 12:25:35 +0300

    fixed burned < 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,16 @@
 package sample;
 
+import javafx.animation.Animation;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
@@ -16,6 +19,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
+import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -36,10 +40,11 @@ public class Controller implements Initializable {
     ScrollPane RightScrollPane;
     @FXML
     Canvas canvas;
+    @FXML
+    ListView<Cell.BedType> FirebedListView;
 
     IntegerProperty calen = new SimpleIntegerProperty();
-    boolean[][]ca;
-    Rectangle[][] cells;
+    Cell[][] cells;
     BooleanProperty runningProperty = new SimpleBooleanProperty(true);
 
 
@@ -48,9 +53,9 @@ public class Controller implements Initializable {
         MakeBindings();
         List<String> rulesList= new ArrayList<>(256);
         for (int i=0;i<256;i++) rulesList.add(Integer.toString(i));
-        calenSlider.setMax(50); calenSlider.setMin(20);
+        calenSlider.setMax(200); calenSlider.setMin(20);
         zoomSlider.setMax(3.125); zoomSlider.setMin(0.125); zoomSlider.setValue(1);
-        calenSlider.setValue(30);
+        calenSlider.setValue(50);
         CreateButton.setOnAction(event -> create());
         StepButton.setOnAction(event -> step());
     }
@@ -61,32 +66,32 @@ public class Controller implements Initializable {
         canvas.setHeight(0);
         canvas.setWidth(0);
         TimeVBox.setVisible(true);
-        cells = new Rectangle[calen.get()][];
-        ca = createCircle(calen.get()/2f, calen.get()/2f, calen.get()/2);
+        cells = new Cell[calen.get()][];
 
         for (int i=0;i<cells.length;i++) {
-            cells[i] = new Rectangle[cells.length];
+            cells[i] = new Cell[cells.length];
             for (int j=0;j<cells.length;j++) {
-                final int ifi = i;
-                final int jfi = j;
-                final Rectangle rect = new Rectangle(16, 16);
-                rect.widthProperty().bind(zoomSlider.valueProperty().multiply(16));
-                rect.heightProperty().bind(zoomSlider.valueProperty().multiply(16));
-                rect.setFill(ca[i][j] ? Color.BLACK : Color.WHITE);
-                rect.setStroke(Color.BLACK);
-                rect.setStrokeWidth(1);
-                //ca[i][j] = false;
-                rect.fillProperty().addListener((observable, oldValue, newValue) -> {
-                    if (((Color) newValue).getRed() == 0) ca[ifi][jfi] = true;
-                    else ca[ifi][jfi] = false;
+                final Cell cell = new Cell(16, 16);
+                cell.widthProperty().bind(zoomSlider.valueProperty().multiply(16));
+                cell.heightProperty().bind(zoomSlider.valueProperty().multiply(16));
+                cell.setStroke(Color.BLACK);
+                cell.setStrokeWidth(1);
+
+                cell.setOnMouseClicked(event1 -> {
+                    Cell.BedType type = FirebedListView.getSelectionModel().getSelectedItem();
+                    Cell source = (Cell) event1.getSource();
+                    source.setBed(type);
                 });
-                rect.setOnMouseClicked(event1 -> {
-                    Rectangle source = (Rectangle) event1.getSource();
-                    Color fill = (Color) source.getFill();
-                    if (fill.getRed() == 0) source.setFill(Color.WHITE);
-                    else source.setFill(Color.BLACK);
+
+                cell.setOnMouseEntered(event1 -> {
+                    if (event1.isControlDown()) {
+                        Cell.BedType type = FirebedListView.getSelectionModel().getSelectedItem();
+                        Cell source = (Cell) event1.getSource();
+                        source.setBed(type);
+                    }
                 });
-                cells[i][j] = rect;
+
+                cells[i][j] = cell;
             }
         }
         TimeVBox.getChildren().clear();
@@ -99,23 +104,10 @@ public class Controller implements Initializable {
         runningProperty.set(false);
     }
 
-    private boolean[][] createCircle(float cx, float cy, int r){
-        boolean[][] canvas = new boolean[2*r+1][];
-        for (int i=0;i<canvas.length;i++){
-            canvas[i]=new boolean[2*r+1];
-            for (int j=0;j<canvas.length;j++) {
-                canvas[i][j]=true;
-                double d = Math.sqrt((i-cy)*(i-cy)+(j-cx)*(j-cx));
-                if (d<=r) canvas[i][j]=false;
-            }
-        }
-        return canvas;
-    }
-
     private void step() {
-        int n = ca.length;
+        int n = cells.length;
         final GraphicsContext gc = canvas.getGraphicsContext2D();
-        if (canvas.getWidth() == 0) {
+        if (canvas.getWidth() == 0) { //Make Canvas of same size
             TimeVBox.setVisible(false);
             canvas.setVisible(true);
             canvas.setWidth(16 * n + 16);
@@ -125,32 +117,43 @@ public class Controller implements Initializable {
         canvas.setHeight(canvas.getHeight() + 16);
         runningProperty.set(true);
 
-        Task<boolean[][]> task = new Task<boolean[][]>() {
+        Task<Cell[][]> task = new Task<Cell[][]>() {
             @Override
-            protected boolean[][] call() throws Exception {
-                boolean[][] newca = new boolean[n][];
-                for (int i = 0; i < n; i++) {
-                    newca[i]=new boolean[n];
-                    for (int j=0;j<n;j++) {
-                        big:
-                        for (int ii=i-1;ii<=i+1;ii++)
-                            for (int jj=j-1;jj<=j+1;jj++) {
-                                if (ii<0||jj<0||ii>=n||jj>=n||ca[ii][jj]) {
-                                    newca[i][j]=true;
-                                    break big;
+            protected Cell[][] call() throws Exception {
+                try {
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n; j++) {
+                            float sum = 0, port, sqrtf = (float) Math.sqrt(2);
+                            for (int ii = i - 1; ii <= i + 1; ii++)
+                                for (int jj = j - 1; jj <= j + 1; jj++) {
+                                    if (ii > 0 && jj > 0 && ii < n && jj < n) {
+                                        port = cells[ii][jj].burned * 1f / 9f;
+                                        if (ii != i && jj != j) port /= sqrtf;
+                                        sum += port;
+                                    }
                                 }
-                            }
+                            //firebed adjustment
+                            float flam=cells[i][j].getFlammablePercentage();
+                            float burned = 0.24f * cells[i][j].getBurned() + sum + flam*sum;
+                            burned = burned > 1f ? 1f : burned;
+                            //float burned = (1f-flam)*0.24f * cells[i][j].getBurned() + flam*sum;
+                            cells[i][j].setNewBurned(burned);
+                        }
                     }
+                    return cells;
+                }catch (Exception ex) {
+                    ex.printStackTrace();
+                    int x=5;
                 }
-                return newca;
+                return null;
             }
         };
         task.setOnSucceeded(event -> {
-            boolean[][] newca = (boolean[][]) event.getSource().getValue();
-            ca=newca;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    gc.setFill(newca[i][j] ? Color.BLACK : Color.WHITE);
+                    cells[i][j].commit();
+                    gc.setFill(cells[i][j].getComputedFill());
+                    //gc.setFill(Color.BLACK);
                     gc.fillRect(16 * j, 16 * i, 16, 16);
                 }
             }
@@ -158,16 +161,44 @@ public class Controller implements Initializable {
         });
         Thread t = new Thread(task);
         t.start();
+
     }
 
     private void MakeBindings() {
 
-        calenTextField.textProperty().bind(Bindings.createStringBinding(() -> Integer.toString((int) calenSlider.getValue()), calenSlider.valueProperty()));
+        calenTextField.textProperty().bind(Bindings.createStringBinding(
+                () -> Integer.toString((int) calenSlider.getValue()),
+                calenSlider.valueProperty())
+        );
         calen.bind(Bindings.createObjectBinding(() -> (int) calenSlider.getValue(), calenSlider.valueProperty()));
         Scale scale=new Scale(1,1);
         scale.xProperty().bind(zoomSlider.valueProperty());
         scale.yProperty().bind(zoomSlider.valueProperty());
         canvas.getTransforms().add(scale);
         StepButton.disableProperty().bind(runningProperty);
+        FirebedListView.setCellFactory(new Callback<ListView<Cell.BedType>, ListCell<Cell.BedType>>() {
+            @Override
+            public ListCell<Cell.BedType> call(ListView<Cell.BedType> param) {
+                return new ListCell<Cell.BedType>() {
+                    @Override
+                    protected void updateItem(Cell.BedType item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) return;
+                        HBox hbox = new HBox();
+                        hbox.setAlignment(Pos.CENTER_RIGHT);
+                        hbox.setSpacing(10);
+                        Label text = new Label(item.name());
+                        Rectangle rect = new Rectangle(20, 20);
+                        rect.setStrokeWidth(1);
+                        rect.setStroke(Color.BLACK);
+                        rect.setFill(Cell.BedPaints[item.ordinal()]);
+                        hbox.getChildren().addAll(text, rect);
+                        setGraphic(hbox);
+                    }
+                };
+
+            }
+        });
+        FirebedListView.setItems(FXCollections.observableArrayList(Cell.BedType.values()));
     }
 }
